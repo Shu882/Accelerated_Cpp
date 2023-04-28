e45dc5ae562c38dc744a20cecb49e7101d61dae3
Alan <zhixueshu@gmail.com>
2023-04-27 22:03:32 -0700

    some changes

--- ch3_grade_mean.cpp
+++ ch3_grade_mean.cpp
@@ -1,21 +1,21 @@
 //
-// Created by Ethan Shu on 4/26/23.
-// Accelerated C++, book example from section 3.1
+// Created by Ethan Shu on 4/27/23.
+// Accelerated C++, book example from section 3.2
 //
 
 #include <iostream>
 #include <string>
 #include <iomanip>
-#include <ios> //input output base
-#include <vector>
+#include <ios> //input output stream base
+#include <algorithm>
 
 using std::cin;
 using std::cout;
 using std::endl;
 using std::string;
-using std::vector;
 using std::setprecision;
 using std::streamsize;
+using std::sort;
 
 int main(){
     string name;
@@ -30,19 +30,22 @@ int main(){
     cout << "Please enter your midterm" << endl;
     cin >> midterm;
 
-    vector<double> hw;
-    double grade;
-    while(cin>>grade) hw.push_back(grade);
-    if (hw.empty()) return 1;
-    sort(hw.begin(), hw.end());
-    vector<double>::size_type n=hw.size();
-    double median = (n%2==0) ? (hw[n/2]+hw[n/2-1])/2 : hw[(n-1)/2];
+    double sum=0, ave_hw;
+    int count = 0;
+    double hw;
 
-    double overall = 0.4*final + 0.2*midterm + 0.4*median;
+    cout << "Please enter all your homework grades, followed by EOF" << endl;
+    while (cin>>hw){
+        sum += hw;
+        ++count;
+    }
+    ave_hw = sum/count;
+
+    double overall;
+    overall = 0.4*final + 0.2*midterm + 0.4*ave_hw;
 
     streamsize prec = cout.precision();
     cout << name << ", your overall grade is: " << setprecision(4) << overall << setprecision(prec) << endl;
 
     return 0;
 }
-
--- ch3_grade_median.cpp
+++ ch3_grade_median.cpp
@@ -1,18 +1,20 @@
 //
-// Created by Ethan Shu on 4/27/23.
-// Accelerated C++, book example from section 3.2
+// Created by Ethan Shu on 4/26/23.
+// Accelerated C++, book example from section 3.1
 //
 
 #include <iostream>
 #include <string>
 #include <iomanip>
-#include <ios> //input output stream base
+#include <ios> //input output base
+#include <vector>
 #include <algorithm>
 
 using std::cin;
 using std::cout;
 using std::endl;
 using std::string;
+using std::vector;
 using std::setprecision;
 using std::streamsize;
 using std::sort;
@@ -30,22 +32,19 @@ int main(){
     cout << "Please enter your midterm" << endl;
     cin >> midterm;
 
-    double sum=0, ave_hw;
-    int count = 0;
-    double hw;
+    vector<double> hw;
+    double grade;
+    while(cin>>grade) hw.push_back(grade);
+    if (hw.empty()) return 1;
+    sort(hw.begin(), hw.end());
+    vector<double>::size_type n=hw.size();
+    double median = (n%2==0) ? (hw[n/2]+hw[n/2-1])/2 : hw[(n-1)/2];
 
-    cout << "Please enter all your homework grades, followed by EOF" << endl;
-    while (cin>>hw){
-        sum += hw;
-        ++count;
-    }
-    ave_hw = sum/count;
-
-    double overall;
-    overall = 0.4*final + 0.2*midterm + 0.4*ave_hw;
+    double overall = 0.4*final + 0.2*midterm + 0.4*median;
 
     streamsize prec = cout.precision();
     cout << name << ", your overall grade is: " << setprecision(4) << overall << setprecision(prec) << endl;
 
     return 0;
 }
+
